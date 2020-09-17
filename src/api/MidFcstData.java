a93c37861a1d11129ed3a0ef2538e80c83b07145
Kay <kimshy9805@gmail.com>
2020-09-17 18:10:12 +0900

    두개 api 클래스 구현 및 해당 자료들 자료구조 update

--- src/api/MidFcstData.java
+++ src/api/MidFcstData.java
@@ -1,6 +1,37 @@
 package api;
 
+import java.util.HashMap;
+
 public class MidFcstData {
+    private final String fcstDate;
+    private final String fcstTime;
+    private HashMap<String, Double> data = new HashMap<String, Double>();
+
+    public MidFcstData (String date, String time) {
+        fcstDate = date;
+        fcstTime = time;
+    }
+
+    public String getFcstDate() {
+        return fcstDate;
+    }
+
+    public String getFcstTime() {
+        return fcstTime;
+    }
+
+    public HashMap<String, Double> getData() {
+        return data;
+    }
+
+
+    public void updateData (String category, Double value) {
+        data.put(category, value);
+    }
+
+
+
+
     //좌료 구조 형태
     //arraylist index가 날짜를 의미할것
     //index 0 -> 3일뒤 am
--- src/api/MidFcstInfoService.java
+++ src/api/MidFcstInfoService.java
@@ -23,13 +23,16 @@ public class MidFcstInfoService {
     private String baseTime;
     private String baseDate;
     //2. api instance
-    private String serviceKey = "7RT79QkWFz0ebm7OOeX1yfoMFfivYjjLNtzb084pv6J9WmgpwDyEzU8nD2FWt%2BcMv58gYrNUYaftZS68w23nkg%3D%3D";
-    private String pageNo = "1";
-    private String numOfRows = "10";
-    private String dataType = "JSON";
-    private String coordinate = "11B00000";
+    private final String serviceKey = "7RT79QkWFz0ebm7OOeX1yfoMFfivYjjLNtzb084pv6J9WmgpwDyEzU8nD2FWt%2BcMv58gYrNUYaftZS68w23nkg%3D%3D";
+    private final StringBuilder urlBuilder1 = new StringBuilder("http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst"); /*URL*/
+    private final StringBuilder urlBuilder2 = new StringBuilder("http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa");
+    private final String pageNo = "1";
+    private final String numOfRows = "10";
+    private final String dataType = "JSON";
+    private String coordinate = "11B10101";
     private String time;
     private int type;
+
 
 
     //constructor
@@ -37,6 +40,7 @@ public class MidFcstInfoService {
         gettingBaseTime(timework.currDate());
         this.time = baseDate + "" + baseTime;
         run();
+        run2();
 
     }
 
@@ -114,20 +118,12 @@ public class MidFcstInfoService {
         JSONObject parse_body = (JSONObject) parse_response.get("body");
         JSONObject parse_items = (JSONObject) parse_body.get("items");
         JSONArray parse_item = (JSONArray) parse_items.get("item");
-
-
         JSONObject weather; // parse_item은 배열형태이기 때문에 하나씩 데이터를 하나씩 가져올때 사용
-
-        // TODO : weather을 이용해서 parse시키는데 시간대로별로 다달라서 시간대별로 sort시켜야함
-        // TODO : treemap사용해서 sort 문제 해결
-        // 다른 api하나 돌리고
-        // 자료구조 저장
 
         weather = (JSONObject) parse_item.get(0);
 
         TreeMap<Object, Object> state = new TreeMap<>();
         TreeMap<Object, Object> prob = new TreeMap<>();
-
 
         //iterating over weather
         for (Object keyStr : weather.keySet()) {
@@ -140,33 +136,19 @@ public class MidFcstInfoService {
 
         }
 
-        System.out.println(state);
-        System.out.println(prob);
+//        System.out.println(state);
+//        System.out.println(prob);
     }
-
-
-    //return url
-    private StringBuilder gettingURL (int type) throws UnsupportedEncodingException {
-        if (type == 1)
-            StringBuilder urlBuilder = new Stringbuilder("http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst"); /*URL*/
-//        else
-//            StringBuilder urlBuilder = new Stringbuilder("http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa"); /*URL*/
-//
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(dataType, "UTF-8")); /*요청자료형식(XML/JSON)Default: JSON*/
-        urlBuilder.append("&" + URLEncoder.encode("regId", "UTF-8") + "=" + URLEncoder.encode(coordinate, "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("tmFc","UTF-8") + "=" + URLEncoder.encode(time, "UTF-8") + "&");
-        return urlBuilder;
-    }
-
-
 
     //중기기온조회 api => return 최저, 최고 기온
-    private void run2() throws IOException {
+    //하한 상한은 필요없음
+    private void run2() throws IOException, ParseException {
+        //initialization
+        ArrayList<MidFcstData> ret = new ArrayList<>(16);
         type = 2;
         URL url = new URL(gettingURL(type).toString());
+
+        //URL work
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
@@ -183,7 +165,8 @@ public class MidFcstInfoService {
         }
         rd.close();
         conn.disconnect();
-        System.out.println(sb);
+
+        //parsing
         String toBeParsed = sb.toString();
         JSONParser parser = new JSONParser();
         JSONObject obj = (JSONObject) parser.parse(toBeParsed);
@@ -191,15 +174,100 @@ public class MidFcstInfoService {
         JSONObject parse_body = (JSONObject) parse_response.get("body");
         JSONObject parse_items = (JSONObject) parse_body.get("items");
         JSONArray parse_item = (JSONArray) parse_items.get("item");
+        JSONObject weather; // parse_item은 배열형태이기 때문에 하나씩 데이터를 하나씩 가져올때 사용
+
+        weather = (JSONObject) parse_item.get(0);
+
+        ArrayList<Node> temp = new ArrayList<>();
+        TreeMap<Node, Object> tree = new TreeMap<>(new SortByDay());
+
+
+        //음.... tree에 모든값이 들어가 있지가 않아용... 해결해야해용
+        for (Object keyStr: weather.keySet()) {
+            Object value = weather.get(keyStr);
+            String key = keyStr.toString();
+            if (key.contains("High") || key.contains("Low") || key.contains("regId")) continue;
+
+            Node node = new Node (key);
+            temp.add(node);
+            tree.put(node, value);
+
+        }
+
+        for (Map.Entry<Node, Object> entry : tree.entrySet()) {
+            System.out.println(entry.getKey().getState());
+            System.out.println(entry.getKey().getDay());
+            System.out.println(entry.getValue());
+            System.out.println("--------");
+        }
+
+
+
+//        for (Map.Entry<Object, Object> entry : tempMax.entrySet()) {
+//            //현재 date +3 + 4 +%
+//            String category = entry.getKey().toString();
+//            Long value = (Long) entry.getValue();
+//
+//            if (ret.isEmpty()) {
+//                ret.add(new MidFcstData())
+//                ret.get(0).updateData(category,value);
+//            }
 
 
     }
 
-    //종합을해서 data 구조를 만들꺼냐 너가 말한 data class
-        //gui에 써먹을꺼냐?
-        //날짜에 따라 상태?가 달라짐 어떤건 am/ pm 8일부터는 하나로 통합이됨.
-        //자료형은 만들고
-        //
+
+
+    //return url
+    private StringBuilder gettingURL (int type) throws UnsupportedEncodingException {
+        StringBuilder urlBuilder = new StringBuilder();
+        if (type == 1)
+            urlBuilder.append(urlBuilder1);
+        else
+            urlBuilder.append(urlBuilder2);
+
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(dataType, "UTF-8")); /*요청자료형식(XML/JSON)Default: JSON*/
+        urlBuilder.append("&" + URLEncoder.encode("regId", "UTF-8") + "=" + URLEncoder.encode(coordinate, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("tmFc","UTF-8") + "=" + URLEncoder.encode(time, "UTF-8") + "&");
+
+        return urlBuilder;
+    }
+
+
+
+    public class Node {
+        private String state;
+        private Integer day = -1;
+
+        public Node (String key) {
+            state = key;
+            differentiate(state);
+        }
+
+        public void differentiate (String state){
+            String diff = state;
+            diff = diff.replaceAll("[^\\d]", " ").trim();
+            day = Integer.parseInt(diff);
+        }
+
+        public String getState() { return state;}
+        public Integer getDay() { return day;}
+
+    }
+
+    public class SortByDay implements Comparator<Node> {
+        @Override
+        public int compare(Node o1, Node o2) {
+            if (o1.getDay() > o2.getDay()) return 1;
+            else if (o1.getDay() < o2.getDay()) return -1;
+            else return 0;
+        }
+
+
+    }
 
 
 }
